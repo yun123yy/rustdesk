83ce4d73dabcd45587192bb8cec01d7d0aa5bc4f
yun123yy <coolpeng333@163.com>
2025-05-25 09:57:21 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://218.27.16.117:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
